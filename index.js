1f2e2464b1aaca97aed2431c156cb997e2b38879
Katy Morton <krokmorton@gmail.com>
2019-07-23 20:26:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var name = "Joe"
 var height = "74"
 
-
-// Don't worry about this
-module.exports = { name, height
+if (name === "Susam") 
+{console.log ("The name is correct")
 }
 
+//Don't worry about this//
+module.exports = { name, height
+}
